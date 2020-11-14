591d4f79702cc6a39444f495de49d8673fea06d0
Chakradhar Rao <chakradhar.jonagam@biqmind.com>
2020-11-14 10:56:40 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -1256,17 +1256,18 @@ const wait = __webpack_require__(470);
 async function run() {
   try {
     const version = core.getInput('version');
-    const kubeconfig_location="/tmp/output/config";
+    const kubeconfig_location="/tmp/output/kubeconfig-"+version+".yaml";
     console.log(`storing kubeconfig here ${kubeconfig_location}!`); 
+      
     await exec.exec('docker', ["run","-d","--privileged","--name=k3s-"+version,
     "-e","K3S_KUBECONFIG_OUTPUT="+kubeconfig_location,
     "-e","K3S_KUBECONFIG_MODE=666",
-    "-v","/tmp/output:/home/runner/.kube","-p","6443:6443",
+    "-v","/tmp/output:/tmp/output","-p","6443:6443",
     "rancher/k3s:"+version,"server"]);
     await wait(parseInt(10000));
     
     core.setOutput("kubeconfig", kubeconfig_location);
-    //core.exportVariable('KUBECONFIG', kubeconfig_location);
+    core.exportVariable('KUBECONFIG', kubeconfig_location);
 
   } catch (error) {
     core.setFailed(error.message);
