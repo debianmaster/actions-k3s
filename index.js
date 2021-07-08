8a2346fd754a7f12daae51f8fc202be5719c6dd5
chakra <chakra>
2021-07-08 16:08:50 +0800

    update

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ async function run() {
     "-e","K3S_KUBECONFIG_MODE=666",
     "-v","/tmp/output:/tmp/output","-p","6443:6443",
     "rancher/k3s:"+version,"server"]);
+
+    output=await exec.exec('kubectl', ["get","nodes"]);
+    console.log(output);
     await wait(parseInt(10000));
     
     core.setOutput("kubeconfig", kubeconfig_location);
