75d3de81b9d0ab7aa284d2112c0344e52f6b3750
chakra <chakra>
2021-07-08 16:36:03 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -1889,7 +1889,7 @@ async function run() {
     "rancher/k3s:"+version,"server"]);
     core.exportVariable('KUBECONFIG', kubeconfig_location);
     core.setOutput("kubeconfig", kubeconfig_location);    
-    await exec.exec('./check-if-cluster-ready.sh');            
+    await exec.exec('sh','./check-if-cluster-ready.sh');            
   } catch (error) {
     core.setFailed(error.message);
   }
