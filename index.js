3ae3d6451e19589dea537fb1e9f99cdbb4cf4a27
chakra <chakra>
2021-07-08 16:56:22 +0800

    update

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ async function run() {
     const version = core.getInput('version');
     const kubeconfig_location="/tmp/output/kubeconfig-"+version+".yaml";
     console.log(`storing kubeconfig here ${kubeconfig_location}!`); 
-      
+
     await exec.exec('docker', ["run","-d","--privileged","--name=k3s-"+version,
     "-e","K3S_KUBECONFIG_OUTPUT="+kubeconfig_location,
     "-e","K3S_KUBECONFIG_MODE=666",
     "-v","/tmp/output:/tmp/output","-p","6443:6443",
     "rancher/k3s:"+version,"server"]);
+    
     await wait(parseInt(10000));
     core.exportVariable('KUBECONFIG', kubeconfig_location);
     core.setOutput("kubeconfig", kubeconfig_location);   
