c08428c7e078412ca88a5e5071ea31aca481dd5c
Sanjito Kurniawan <s4nji@users.noreply.github.com>
2021-11-12 02:17:55 +0100

    Detect remote docker host (#11)
    * Expose port 6443
    
    * Fetch kubeconfig if using remote docker host
    
    * Build

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const wait = require('./wait');
+const fs = require('fs');
 
 async function run() {
   try {
@@ -9,12 +10,21 @@ async function run() {
     console.log(`storing kubeconfig here ${kubeconfig_location}!`); 
 
     await exec.exec('docker', ["run","-d","--privileged","--name=k3s-"+version,
+    "--expose=6443",
     "-e","K3S_KUBECONFIG_OUTPUT="+kubeconfig_location,
     "-e","K3S_KUBECONFIG_MODE=666",
     "-v","/tmp/output:/tmp/output","-p","6443:6443","-p","80:80","-p","443:443","-p","8080:8080",
     "rancher/k3s:"+version,"server"]);
     
     await wait(parseInt(10000));
+
+    if (process.env.DOCKER_HOST) {
+      const kubeconfig = await exec.getExecOutput('docker', ["exec", `k3s-${version}`, "cat", kubeconfig_location]);
+
+      await exec.exec('mkdir', ['/tmp/output']);
+      fs.writeFileSync(kubeconfig_location, kubeconfig.stdout);
+    }
+
     core.exportVariable('KUBECONFIG', kubeconfig_location);
     core.setOutput("kubeconfig", kubeconfig_location);   
     const nodeName=await exec.getExecOutput("kubectl get nodes --no-headers -oname");    
